6904d2958714149d18dbedd0b0198c8dc8963a80
Giovanni Walter <giovanni.walter@outlook.com>
2025-05-29 10:26:16 -0300

    removed postgres and redis services

--- hosts/shared.nix
+++ hosts/shared.nix
@@ -101,24 +101,6 @@
 
   # services
   services = {
-    redis = {
-      servers = {
-        "newstrading" = {
-          enable = true;
-          port = 6379;
-        };
-      };
-    };
-    postgresql = {
-      enable = true;
-      ensureDatabases = [ "postgres" ];
-      authentication = pkgs.lib.mkOverride 10 ''
-        #type database  DBuser  auth-method
-        local all       all     trust
-        host  all       all     127.0.0.1/32   trust
-        host  all       all     ::1/128        trust
-      '';
-    };
     pulseaudio.enable = false;
     pipewire = {
       enable = true;
