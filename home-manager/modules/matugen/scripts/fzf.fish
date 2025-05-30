a8c875f99a2cd94064c42141a78fc8cee0ada445
Giovanni Walter <giovanni.walter@outlook.com>
2025-05-30 09:54:59 -0300

    minor update

--- home-manager/modules/matugen/scripts/fzf.fish
+++ home-manager/modules/matugen/scripts/fzf.fish
@@ -1,4 +1,4 @@
-#!/bin/fish
+#!/bin/env fish
 set -Ux FZF_DEFAULT_OPTS "\
   --color=bg+:#423733,bg:#1a120e,spinner:#d2c78f,hl:#ffb595 \
   --color=fg:#f1dfd8,header:#ffb595,info:#d2c78f,pointer:#f1dfd8 \
