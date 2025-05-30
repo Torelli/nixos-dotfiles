233869eff3c518183141d43960d9f695cc3f5fc6
Giovanni Walter <giovanni.walter@outlook.com>
2025-05-30 10:48:41 -0300

    minor update

---
+++ home-manager/modules/fish/functions/control-l.fish
@@ -0,0 +1,11 @@
+function control-l --wraps=clear --description 'alias clear control-l'
+  clear
+end
+
+function ctrl-l -wraps=clear --description 'alias clear ctrl-l'
+  clear
+end
+
+function C-l -wraps=clear --description 'alias clear C-l'
+  clear
+end
