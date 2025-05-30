879ba40d966fb9cb3d6f9071959d0fa184c03079
Giovanni Walter <giovanni.walter@outlook.com>
2025-05-29 22:08:58 -0300

    minor update

--- home-manager/modules/matugen/default.nix
+++ home-manager/modules/matugen/default.nix
@@ -6,12 +6,12 @@
 {
   home.file."/home/${username}/.config/matugen/config.toml".source = ./config.toml;
 
-  home.file = {
+  home.file."home/${username}/.config/matugen/scripts" = {
     source = ./scripts;
     recursive = true;
   };
 
-  home.file = {
+  home.file."home/${username}/.config/matugen/templates" = {
     source = ./templates;
     recursive = true;
   };
