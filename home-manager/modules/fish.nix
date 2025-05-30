f3e99165a14c091cc0972aaf205814d188fb93f2
Giovanni Walter <giovanni.walter@outlook.com>
2025-05-30 17:54:55 -0300

    updated fish

--- home-manager/modules/fish.nix
+++ home-manager/modules/fish.nix
@@ -2,7 +2,7 @@
   programs = {
     direnv = {
       enable = true;
-      enableBashIntegration = true; # see note on other shells below
+      enableFishIntegration = true; # see note on other shells below
       nix-direnv.enable = true;
     };
 
